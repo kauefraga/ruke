262e0b7ef4c1db6cf02511a36a9c0b4a5faab43f
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-15 16:45:13 -0300

    feat: use rukefile module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
-use std::process::exit;
-
 mod cli;
+mod rukefile;
+
+use rukefile::{
+    execute_task,
+    get_rukefile
+};
 
 fn main() {
-    /*
-     * 1. rufile -> procurar makefile na raiz e executar o target main se existir
-     * 1.1 rufile {target} -> mesma coisa porém executar o target
-     * 1.2 rufile ... --quiet -q -> mesma coisa porém sem saída (apenas executar)
-     * 1.3 rufile ... --file {file} -f {file} -> passando o arquivo
-     * 1.4 rufile --version -v -> versão
-     */
-
     let args = cli::get_arguments();
 
     if let None = args.get(0) {
-        println!("executando target main...");
-        exit(0)
+        let rukefile = get_rukefile("");
+
+        match rukefile {
+            Ok(rukefile) => {
+                println!("{}", rukefile);
+                execute_task("main");
+            },
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+
+        return;
     }
 
     match args[0].as_str() {
         "--help" | "-h" => cli::show_help(),
         "--version" | "-v" => cli::show_version(),
         arg => {
-            // pegar target
-            println!("{arg}")
+            let rukefile = get_rukefile("");
+
+            match rukefile {
+                Ok(rukefile) => {
+                    println!("{}", rukefile);
+                    execute_task(arg);
+                },
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
+
+            return;
         },
     }
 }
