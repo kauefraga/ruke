1d24b090317678ec7d8717cf15508d593e40cc70
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-17 21:11:55 -0300

    feat: create Flag struct, get_flags function and apply correct indentation

--- crates/ruke_cli/src/flags.rs
+++ crates/ruke_cli/src/flags.rs
@@ -1,3 +1,48 @@
+pub struct Flag {
+    pub name: String,
+    pub value: Option<String>
+}
+
+pub fn get_flags(arguments: &Vec<String>) -> Option<Vec<Flag>> {
+    let mut flags = Vec::<Flag>::with_capacity(arguments.len());
+    let mut arguments = arguments
+        .iter()
+        .peekable();
+
+    while let Some(argument) = arguments.next() {
+        if argument.contains("-") {
+            if let Some(value) = arguments.peek() {
+                if value.contains("-") {
+                    flags.push(Flag{
+                        name: argument.replace("-", ""),
+                        value: None
+                    });
+
+                    continue;
+                }
+
+                flags.push(Flag{
+                    name: argument.replace("-", ""),
+                    value: Some(value.to_string())
+                });
+
+                continue;
+            }
+
+            flags.push(Flag{
+                name: argument.replace("-", ""),
+                value: None
+            });
+        }
+    }
+
+    if flags.len() == 0 {
+        return None;
+    }
+
+    Some(flags)
+}
+
 pub fn show_help() {
     println!("A dead-simple automation tool.\n");
 
@@ -6,20 +51,20 @@ pub fn show_help() {
 
     println!("Examples:");
     println!(
-      "  {}\n  {}\n  {}\n  {}\n",
-      "ruke",
-      "ruke build",
-      "ruke release --quiet",
-      "ruke dev --file path/to/rufile"
+        "  {}\n  {}\n  {}\n  {}\n",
+        "ruke",
+        "ruke build",
+        "ruke release --quiet",
+        "ruke dev --file path/to/rufile"
     );
 
     println!("Flags:");
     println!(
-      "  {}\n  {}\n  {}\n  {}",
-      "-h, --help        help for ruke",
-      "-v, --version     version for ruke",
-      "-q, --quiet       execute target silently (no output)",
-      "-f, --file FILE   read FILE as rufile"
+        "  {}\n  {}\n  {}\n  {}",
+        "-h, --help        help for ruke",
+        "-v, --version     version for ruke",
+        "-q, --quiet       execute target silently (no output)",
+        "-f, --file FILE   read FILE as rufile"
     );
 }
 
