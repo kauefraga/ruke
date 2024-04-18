bf9c7c818273a90e091a3877774d564626704d9a
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-18 18:56:14 -0300

    feat: create a CLI using Clap

---
+++ src/cli.rs
@@ -0,0 +1,14 @@
+use clap::{arg, ArgMatches, Command};
+
+pub fn get_matches() -> ArgMatches {
+    let cli = Command::new("ruke")
+        .author("Kauê Fraga Rodrigues")
+        .version("0.1.0")
+        .about("A dead-simple automation tool. Inspired by Makefile and Justfile.")
+        .arg_required_else_help(true)
+        .arg(arg!([target] "Sets the target task").default_value("main"))
+        .arg(arg!(-f --file <FILE> "Sets a ruke.toml to use").default_value("Ruke.toml"))
+        .arg(arg!(-q --quiet "Sets run to be silent"));
+
+    cli.get_matches()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli::get_matches();
+
+    let target = matches.get_one::<String>("target");
+    let rukefile = matches.get_one::<String>("file");
+    let quiet = matches.get_one::<bool>("quiet");
+
+    println!("{:?}", rukefile);
+    println!("{:?}", target);
+    println!("{:?}", quiet);
 }
