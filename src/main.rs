702ffcc6bd9cbb07c36cd416537d687b338489f7
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-20 10:58:45 -0300

    feat: remove verifications and print target's recipe

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,17 @@ use tasks::Rukefile;
 fn main() {
     let matches = cli::get_matches();
 
-    let _target = matches.get_one::<String>("target");
-    let file = matches.get_one::<String>("file");
+    let target = matches.get_one::<String>("target").expect("required target");
+    let file = matches.get_one::<String>("file").expect("required file");
     let _quiet = matches.get_one::<bool>("quiet");
 
-    if let Some(file) = file {
-        let rukefile = fs::read_to_string(file).expect("invalid path");
-        let rukefile = Rukefile::from_str(&rukefile);
+    let rukefile = fs::read_to_string(file).expect("invalid path");
+    let rukefile = Rukefile::from_str(&rukefile).expect("invalid toml");
 
-        rukefile.unwrap().tasks.iter().for_each(|task| println!("{:?}", task));
-    }
+    rukefile.tasks.iter()
+        .for_each(|recipe| println!("{:?}", recipe));
+
+    let recipe = rukefile.find_recipe(target.to_string());
+
+    println!("{:?}", recipe);
 }
