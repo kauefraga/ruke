0c38c0084b1b71fa9bfc22bf09d0b0527d3f2da6
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-20 10:22:35 -0300

    refactor: prefix unused variables with _ and print each recipe

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@ mod tasks;
 
 use std::fs;
 
-use tasks::get_tasks;
+use tasks::Rukefile;
 
 fn main() {
     let matches = cli::get_matches();
 
-    let target = matches.get_one::<String>("target");
+    let _target = matches.get_one::<String>("target");
     let file = matches.get_one::<String>("file");
-    let quiet = matches.get_one::<bool>("quiet");
+    let _quiet = matches.get_one::<bool>("quiet");
 
     if let Some(file) = file {
         let rukefile = fs::read_to_string(file).expect("invalid path");
+        let rukefile = Rukefile::from_str(&rukefile);
 
-        println!("{:?}", get_tasks(rukefile));
+        rukefile.unwrap().tasks.iter().for_each(|task| println!("{:?}", task));
     }
 }
