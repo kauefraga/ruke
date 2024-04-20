db7c01e20e7b5c48f74e1e6a28de955d453f39d3
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-20 16:55:40 -0300

    refactor: remove file default target

--- src/cli.rs
+++ src/cli.rs
@@ -6,7 +6,7 @@ pub fn get_matches() -> ArgMatches {
         .version("0.1.0")
         .about("A dead-simple automation tool. Inspired by Makefile and Justfile.")
         .arg(arg!([target] "Sets the target task").default_value("main"))
-        .arg(arg!(-f --file <FILE> "Sets a ruke.toml to use").default_value("Ruke.toml"))
+        .arg(arg!(-f --file <FILE> "Sets a Ruke.toml or Rukefile to use"))
         .arg(arg!(-q --quiet "Sets run to be silent"));
 
     cli.get_matches()
