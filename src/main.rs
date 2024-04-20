70db5763bce51f939fa029403bd645d2c9914956
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-20 13:54:51 -0300

    feat: use run recipe

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,10 @@ fn main() {
 
     let target = matches.get_one::<String>("target").expect("required target");
     let file = matches.get_one::<String>("file").expect("required file");
-    let _quiet = matches.get_one::<bool>("quiet");
+    let quiet = matches.get_one::<bool>("quiet").unwrap();
 
     let rukefile = fs::read_to_string(file).expect("invalid path");
     let rukefile = Rukefile::from_str(&rukefile).expect("invalid toml");
 
-    rukefile.tasks.iter()
-        .for_each(|recipe| println!("{:?}", recipe));
-
-    let recipe = rukefile.find_recipe(target.to_string());
-
-    println!("{:?}", recipe);
+    rukefile.run_recipe(target.to_string(), *quiet);
 }
