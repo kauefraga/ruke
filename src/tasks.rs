7b1620848e8c351b807325d9a63ac0c6af87bb0e
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-20 10:57:30 -0300

    feat: implement find_recipe function

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,24 +1,27 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 pub struct Recipe {
     pub name: String,
     pub command: String,
     pub arguments: Option<Vec<String>>
 }
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize)]
 pub struct Rukefile {
     pub tasks: Vec<Recipe>
 }
 
 impl Rukefile {
-    pub fn from_str(raw: &str) -> Option<Rukefile> {
-        let rukefile = toml::from_str::<Rukefile>(raw);
+    pub fn from_str(raw: &str) -> Result<Self, toml::de::Error> {
+        toml::from_str::<Rukefile>(raw)
+    }
 
-        match rukefile {
-            Ok(rukefile) => Some(rukefile),
-            Err(_) => None
-        }
+    pub fn find_recipe(&self, name: String) -> Option<Recipe> {
+        let recipe = self.tasks
+            .iter()
+            .find(|recipe| recipe.name.eq(&name));
+
+        return recipe.cloned();
     }
 }
