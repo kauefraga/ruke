fb99e4c9b76baa908cf805b42dd2624bf5e191b9
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-04-29 19:17:30 -0300

    refactor: remove match statement and verify if Some(path) exists

--- src/tasks/path.rs
+++ src/tasks/path.rs
@@ -16,21 +16,26 @@ fn resolve_file(root_path: PathBuf) -> Option<PathBuf> {
 }
 
 pub fn resolve_path(path: Option<&String>) -> Option<PathBuf> {
-    match path {
-        Some(path) => Some(PathBuf::from(path)),
-        None => {
-            let current_directory = env::current_dir().ok()?;
-            let path_ancestors = current_directory.ancestors();
+    if let Some(path) = path {
+        let path = PathBuf::from(path);
 
-            for path in path_ancestors {
-                let path = path.to_path_buf();
+        if path.exists() {
+            return Some(path);
+        }
 
-                if let Some(resolved_file) = resolve_file(path) {
-                    return Some(resolved_file);
-                }
-            }
+        return None;
+    }
 
-            None
+    let current_directory = env::current_dir().ok()?;
+    let path_ancestors = current_directory.ancestors();
+
+    for path in path_ancestors {
+        let path = path.to_path_buf();
+
+        if let Some(resolved_file) = resolve_file(path) {
+            return Some(resolved_file);
         }
     }
+
+    None
 }
