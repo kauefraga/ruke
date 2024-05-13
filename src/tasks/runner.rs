81114fd00faf51447918949a4a6cf74b335e06d2
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-05-13 16:13:48 -0300

    refactor: improve error covering and remove match statement

--- src/tasks/runner.rs
+++ src/tasks/runner.rs
@@ -24,34 +24,38 @@ pub fn run_task(task: Task, quiet: bool) {
             .map(|argument| argument.to_string())
             .collect::<Vec<String>>();
 
-        let output = Command::new(command[0])
-            .args(arguments)
-            .output()
-            .expect("Failed to execute command");
+        let output = Command::new(command[0]).args(arguments).output();
 
-        let is_success_and_not_quiet = output.status.success() && !quiet;
-
-        match is_success_and_not_quiet {
-            true => {
-                println!(
-                    "{} {}",
-                    "$".color(Colors::MagentaFg),
-                    command.join(" ").color(Colors::BrightBlackFg)
-                );
-
-                let stdout = String::from_utf8_lossy(&output.stdout);
-
-                if stdout.is_empty() {
-                    println!("{}", "Task completed!".color(Colors::GreenFg));
-                    return;
-                }
-
-                println!("{}\n", stdout.trim_end());
-            }
-            false => {
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                eprintln!("{}", stderr.trim_end());
-            }
+        if output.is_err() {
+            eprintln!(
+                "{}",
+                format!("Failed executing the command `{}`.", command.join(" "))
+                    .color(Colors::RedFg)
+            );
+            return;
         }
+
+        let output = output.unwrap();
+
+        if quiet || !output.status.success() {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            eprintln!("{}", stderr.trim_end());
+            return;
+        }
+
+        println!(
+            "{} {}",
+            "$".color(Colors::MagentaFg),
+            command.join(" ").color(Colors::BrightBlackFg)
+        );
+
+        let stdout = String::from_utf8_lossy(&output.stdout);
+
+        if stdout.is_empty() {
+            println!("{}", "Task completed!".color(Colors::GreenFg));
+            return;
+        }
+
+        println!("{}\n", stdout.trim_end());
     }
 }
