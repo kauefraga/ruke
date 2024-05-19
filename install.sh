38d2b7195cd98cdf4e5eed6f00396d93e31e3bf1
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-05-18 21:07:36 -0300

    script: move the install script to the root

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-curl -OLs https://github.com/kauefraga/ruke/releases/download/v0.1.8/ruke-x86_64-unknown-linux-gnu.tar.gz
+curl -OLs https://github.com/kauefraga/ruke/releases/latest/download/ruke-x86_64-unknown-linux-gnu.tar.gz
 
 tar -zxvf ruke-x86_64-unknown-linux-gnu.tar.gz
+
+rm ruke-x86_64-unknown-linux-gnu.tar.gz
 
 echo 'Add ruke in your PATH or move it to a local where PATH is already set'
 echo '$ echo "mv ruke /usr/bin/"'
